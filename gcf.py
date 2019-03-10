3e04c02ee0f17a9de52429ad2201789a1161c9ea
Alexander Rodgers <alexedrodgers@gmail.com>
2019-03-10 03:09:16 -0700

    Added my version of a gcf question

---
+++ gcf.py
@@ -0,0 +1,29 @@
+test1 = [12, 16, 24]
+test2 = [9, 81, 45, 90]
+test3 = [100, 25, 60, 180]
+
+def gcf(nums):
+    min = 0
+    for i in range(1, len(nums)):
+        if nums[min] > nums[i]:
+            min = i
+    start = nums[min]
+    max = len(nums)
+    j = 0
+    done = False
+    while(not done):
+        while(j < max):
+            if nums[j] / start == nums[j] // start:
+                if j + 1 == max:
+                    return start
+                j += 1
+            else:
+                if start == 1:
+                    return start
+                # we have to reset if the factor isn't the same across the list
+                j = 0
+                start -= 1
+
+print(gcf(test1))
+print(gcf(test2))
+print(gcf(test3))
