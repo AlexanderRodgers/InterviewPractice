751e87ce0223af86dcc3a5aa544aa080831c9ae2
Alexander Rodgers <alexedrodgers@gmail.com>
2019-03-10 03:10:27 -0700

    Added time complexity

--- gcf.py
+++ gcf.py
@@ -2,7 +2,7 @@ test1 = [12, 16, 24]
 test2 = [9, 81, 45, 90]
 test3 = [100, 25, 60, 180]
 
-def gcf(nums):
+def gcf(nums): # O(n) time
     min = 0
     for i in range(1, len(nums)):
         if nums[min] > nums[i]:
@@ -11,7 +11,7 @@ def gcf(nums):
     max = len(nums)
     j = 0
     done = False
-    while(not done):
+    while(not done): #O(n^2) time
         while(j < max):
             if nums[j] / start == nums[j] // start:
                 if j + 1 == max:
@@ -23,6 +23,8 @@ def gcf(nums):
                 # we have to reset if the factor isn't the same across the list
                 j = 0
                 start -= 1
+
+# total efficiency is O(n + n^2) => O(n^2)
 
 print(gcf(test1))
 print(gcf(test2))
